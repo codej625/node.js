80a55d62535b073c179d19532d0dee3c8eb9862a
codej625 <codej625@gmail.com>
2024-03-03 23:24:43 +0900

    20240303 ascii-art update

--- toy-project/router.js
+++ toy-project/router.js
@@ -1,23 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const figlet = require("figlet");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const path = require('path');
-const userController = require('./controller/user-controller');
-
+const path = require("path");
+const userController = require("./controller/user-controller");
 const port = process.env.PORT || 8080;
-app.listen(port, () => { console.log(`port: ${port}`);});
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   try {
-    res.sendFile(path.join(__dirname, 'public/html', 'index.html'));
+    res.sendFile(path.join(__dirname, "public/html", "index.html"));
   } catch (error) {
-    res.status(500).send('server error');
+    res.status(500).send("server error");
   }
 });
 
-app.use('/user', userController);
+app.use("/user", userController);
+
+app.listen(port, () => {/* console.log(`port: ${port}`); */});
+
+figlet(`odej625 Server Port: ${port}`, function (err, data) {
+  if (err) {
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
