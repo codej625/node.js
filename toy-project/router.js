02635d5179cb87022e4faaf23a165c867dd3deaa
codej625 <codej625@gmail.com>
2024-03-07 23:58:53 +0900

    20240307 update

--- toy-project/router.js
+++ toy-project/router.js
@@ -1,16 +1,21 @@
+/* 모듈을 불러온다.  */
 const express = require("express");
 const figlet = require("figlet");
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config(); /* 환경 변수를 관리하는데 사용되는 모듈 */
+/* 유저 컨트롤러를 불러온다. */
+const userController = require("./controller/user-controller");
 
 const app = express();
 const path = require("path");
-const userController = require("./controller/user-controller");
 const port = process.env.PORT || 8080;
 
+/* 정적 파일 제공 설정 */
 app.use(express.static(path.join(__dirname, "public")));
+/* CORS 미들웨어 설정 */
 app.use(cors());
 
+/* 루트 경로 처리 */
 app.get("/", (req, res) => {
   try {
     res.sendFile(path.join(__dirname, "public/html", "index.html"));
@@ -18,9 +23,10 @@ app.get("/", (req, res) => {
     res.status(500).send("server error");
   }
 });
-
+/* 사용자 컨트롤러 라우터 설정 */
 app.use("/user", userController);
 
+/* 서버 시작 및 포트 리스닝 */
 app.listen(port, () => {
   figlet(`odej625 Server Port: ${port}`, function (err, data) {
     if (err) {
