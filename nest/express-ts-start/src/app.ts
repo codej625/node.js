b0dc752629e5b86e923cba5e0c90795c3aed5aea
codej625 <codej625@gmail.com>
2024-12-05 22:25:45 +0900

    201205 Update

--- nest/express-ts-start/src/app.ts
+++ nest/express-ts-start/src/app.ts
@@ -1,1 +1,12 @@
-console.log("hello world!!");
+import * as express from "express";
+const app: express.Express = express();
+const port: number = 8000;
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  console.log(req.query);
+  res.send({ 'name': 'codej625' });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
