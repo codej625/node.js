8766dca56179eec284182be682403f7dc2931f87
codej625 <codej625@gmail.com>
2024-12-07 19:56:26 +0900

    241207 Added data mocking by codej625

--- nest/express-ts-start/src/app.ts
+++ nest/express-ts-start/src/app.ts
@@ -1,10 +1,12 @@
-import * as express from "express";
+import * as express from 'express';
+import { Cat } from './app.model';
 const app: express.Express = express();
 const port: number = 8000;
 
 app.get('/', (req: express.Request, res: express.Response) => {
   console.log(req.query);
-  res.send({ 'name': 'codej625' });
+  // Test
+  res.send({ cats: Cat });
 })
 
 app.listen(port, () => {
