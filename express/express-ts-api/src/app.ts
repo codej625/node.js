464b7f2590a1f57053fae61976f745e3cbf65515
codej625 <codej625@gmail.com>
2024-12-09 08:50:45 +0900

    241209 Cats module separated by codej625

--- express/express-ts-api/src/app.ts
+++ express/express-ts-api/src/app.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { Cat, CatType } from './app.model';
+import catsRouter from './cats/cats.route';
 
 const app: express.Express = express();
 const port: number = 8000;
@@ -7,81 +7,12 @@ const port: number = 8000;
 // Apply the middleware globally
 app.use(express.json());
 
+app.use(catsRouter);
+
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
   // Logs the request headers
   console.log('\nRequest logging -> ', req.headers);
   next();
-});
-
-// 전체 조회(Read All)
-app.get('/cats', (req: express.Request, res: express.Response) => {
-  try {
-    const cats: CatType[] = Cat;
-    res.status(200).send({
-      success: true,
-      data: {
-        cats
-      }
-    });
-  } catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message
-    });
-  }
-});
-
-// 특정 아이디 조회(Read one)
-app.get('/cats/:id', (req: express.Request, res: express.Response) => {
-  try {
-    const id: string = req.params.id;
-    const cats: CatType | undefined = Cat.find(cat => cat.id === parseInt(id));
-    // 아이디 값이 없을 시
-    if (!cats) throw new Error('Not found cat');
-
-    res.status(200).send({
-      success: true,
-      data: {
-        cats
-      }
-    });
-  }
-  catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message
-    });
-  }
-});
-
-// Create
-app.post('/cats', (req: express.Request, res: express.Response) => {
-  try {
-    const data: CatType | any = req.body;
-
-    const hasFalsyValue: (string | number)[] = Object.values(data);
-    // 입력값이 부족 시
-    hasFalsyValue.forEach(value => {
-      if (!value) throw new Error('Insufficient input');
-    });
-
-    const cats: CatType[] = Cat;
-    // filter가 작동 후 메서드 체인으로 map이 작동
-    let maxId: number = Math.max(...cats.filter(cat => cat.id !== undefined).map(cat => cat.id!));
-    data['id'] = ++maxId;
-    Cat.push(data);
-
-    res.status(200).send({
-      success: true,
-      data: { data }
-    });
-  }
-  catch (error: any) {
-    res.status(400).send({
-      success: false,
-      error: error.message
-    });
-  }
 });
 
 // 404 Page not found
---
+++ express/express-ts-api/src/cats/cats.route.ts
@@ -0,0 +1,105 @@
+import { Request, Response, Router } from 'express';
+import { Cat, CatType } from './cats.model';
+
+const router: Router = Router();
+
+// Read All -> GET
+router.get('/cats', (req: Request, res: Response) => {
+  try {
+    const cats: CatType[] = Cat;
+    res.status(200).send({
+      success: true,
+      data: {
+        cats
+      }
+    });
+  }
+  catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(400).send({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+});
+
+// Read one -> GET
+router.get('/cats/:id', (req: Request, res: Response) => {
+  try {
+    const id: string = req.params.id;
+    const cats: CatType | undefined = Cat.find(cat => cat.id === parseInt(id));
+    // 아이디 값이 없을 시
+    if (!cats) throw new Error('Not found cat');
+
+    res.status(200).send({
+      success: true,
+      data: {
+        cats
+      }
+    });
+  }
+  catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(400).send({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+});
+
+// Create -> POST
+router.post('/cats', (req: Request, res: Response) => {
+  try {
+    const data: CatType | any = req.body;
+
+    const hasFalsyValue: (string | number)[] = Object.values(data);
+    // 입력값이 부족 시
+    hasFalsyValue.forEach(value => {
+      if (!value) throw new Error('Insufficient input');
+    });
+
+    const cats: CatType[] = Cat;
+    // filter가 작동 후 메서드 체인으로 map이 작동
+    let maxId: number = Math.max(...cats.filter(cat => cat.id !== undefined).map(cat => cat.id!));
+    data['id'] = ++maxId;
+    Cat.push(data);
+
+    res.status(200).send({
+      success: true,
+      data: { data }
+    });
+  }
+  catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(400).send({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+});
+
+// Update -> PUT(전체 업데이트)
+
+// Update -> PATCH(부분적으로 업데이트)
+router.patch(('/cats/:id'), (req: Request, res: Response) => {
+  try {
+    res.status(200).send({
+      success: true,
+      data: {}
+    });
+  }
+  catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(400).send({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+});
+
+
+export default router;
