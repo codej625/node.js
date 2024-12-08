2c48a8c80c55abfb65560dceee976773f04f3d09
codej625 <codej625@gmail.com>
2024-12-08 09:49:10 +0900

    241208 The software was updated by codej625

--- nest/express-ts-start/src/app.ts
+++ nest/express-ts-start/src/app.ts
@@ -4,40 +4,45 @@ import { Cat } from './app.model';
 const app: express.Express = express();
 const port: number = 8000;
 
-// 전체에 미들웨어 적용
+// Apply the middleware globally
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.log('logging test -> ', req.rawHeaders[3]);
+  // Logs the request headers
+  console.log('Request logging -> ', req.rawHeaders);
   next();
 });
 
-// 특정 path에 미들웨어 적용
+
+// Example Apply middleware to a specific path
 app.get('/cats/som', (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.log('som');
+  console.log('first');
   next();
 });
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send({ cats: Cat });
-})
+});
 
 app.get('/cats', (req: express.Request, res: express.Response) => {
   res.send({ Cat });
-})
+});
 
 app.get('/cats/blue', (req: express.Request, res: express.Response) => {
   res.send({ blue: Cat[0] });
-})
-
-app.get('/cats/som', (req: express.Request, res: express.Response) => {
-  res.send({ som: Cat[1] });
-})
-
-// 없는 페이지 404처리
-app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.log('404');
-  res.send({ 'error': '404 not found error' })
 });
 
+app.get('/cats/som', (req: express.Request, res: express.Response) => {
+  console.log('second');
+  res.send({ som: Cat[1] });
+});
+
+
+// 404 Page not found
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error('HTTP 404');
+  res.send('404 Page not found');
+});
+
+// Port number
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
